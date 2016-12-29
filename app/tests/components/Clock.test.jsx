bb01fdcc4eb769f7edc2d2ad7f9bbae4b3a3d2fa
Patricia Arbona <patricia.arbona@omadahealth.com>
2016-12-29 01:34:03 -0500

    more testing

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -10,4 +10,25 @@ describe('Clock', () => {
   it("should exist", () => {
     expect(Clock).toExist();
   });
+
+  describe('formatSeconds', () => {
+    it('should format seconds', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 615;
+      var expected = '10:15';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+  describe('formatSeconds', () => {
+    it('should format seconds when min or sec is less than 10', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 61;
+      var expected = '01:01';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+  });
 });
