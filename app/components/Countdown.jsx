eedc1a32bd9d620f8723739a37e7324af9cb5161
Patricia Arbona <patricia.arbona@omadahealth.com>
2016-12-29 21:51:10 -0500

    Render and Style Timer

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+  getInitialState: function() {
+    return {count: 0};
+  },
+  //update state in handlesetcountdown
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds
+    });
+  },
   render: function () {
+    var {count} = this.state;
     return (
       <div>
-        <Clock totalSeconds={129}/>
+        <Clock totalSeconds={count}/>
+        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
       </div>
     );
   }
